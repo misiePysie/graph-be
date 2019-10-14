17fb94bdab558389eb657ed8ca2e28ba60af15c9
GSreniawska <gabriela.sreniawska@gmail.com>
2019-10-14 18:56:48 +0200

    First read and analyse files

--- src/main/java/misiepysie/graph_be/GraphApplication.java
+++ src/main/java/misiepysie/graph_be/GraphApplication.java
@@ -8,6 +8,11 @@ public class GraphApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GraphApplication.class, args);
+
+        ReadFiles importFiles=new ReadFiles();
+        importFiles.listAllFilesNames();
+        ReadFiles.createNodeForEachFile();
+        ReadFiles.getListOfSourceFiles().forEach(x->x.toString());
     }
 
 }
