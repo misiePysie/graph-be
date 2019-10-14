8f20bf7cc7a9284eedae3210e60cfc7a1e568c57
NWojt <n.wojtowicz098@gmail.com>
2019-10-14 22:09:17 +0200

    RestController start

--- src/main/java/misiepysie/graph_be/GraphApplication.java
+++ src/main/java/misiepysie/graph_be/GraphApplication.java
@@ -2,17 +2,26 @@ package misiepysie.graph_be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.io.IOException;
 
 @SpringBootApplication
 public class GraphApplication {
 
+    public static String directoryPath;
+
     public static void main(String[] args) {
         SpringApplication.run(GraphApplication.class, args);
 
+
+        try{
         ReadFiles importFiles=new ReadFiles();
         importFiles.listAllFilesNames();
         ReadFiles.createNodeForEachFile();
         ReadFiles.getListOfSourceFiles().forEach(x->x.toString());
+    }
+        catch(IOException e){
+            System.out.println(e.getStackTrace());
+        }
     }
 
 }
