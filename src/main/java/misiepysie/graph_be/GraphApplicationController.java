af7d9b9775d17d3890667486bf6c7d5aaf30e9ce
NWojt <n.wojtowicz098@gmail.com>
2019-10-15 23:10:14 +0200

    Merge branch 'master' into rest-api

--- src/main/java/misiepysie/graph_be/GraphApplicationController.java
+++ src/main/java/misiepysie/graph_be/GraphApplicationController.java
@@ -1,15 +1,27 @@
 package misiepysie.graph_be;
 
+import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 
 @RestController
 public class GraphApplicationController {
 
-    @RequestMapping(value = "/dir", method = RequestMethod.POST)
-    public String dirPath(@RequestHeader(value = "path") String path) {
-        GraphApplication.directoryPath = path;
+    @CrossOrigin(origins = "http://localhost:3030")
 
-        return GraphApplication.directoryPath;
+    @ResponseBody
+    @RequestMapping(path="/dir", method = RequestMethod.GET)
+    public String dirPath(@RequestBody String paths){
+
+        Gson gson = new Gson();
+
+      DirectoryPath directory = gson.fromJson(paths,DirectoryPath.class);
+
+
+       return "hej";
     }
+
+
 }
