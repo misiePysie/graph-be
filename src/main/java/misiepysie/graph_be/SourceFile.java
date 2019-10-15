322194aadf582c2744239c7f099082c87a05569b
GSreniawska <gabriela.sreniawska@gmail.com>
2019-10-15 10:22:27 +0200

    Fix reading size of file

--- src/main/java/misiepysie/graph_be/SourceFile.java
+++ src/main/java/misiepysie/graph_be/SourceFile.java
@@ -1,13 +1,12 @@
 package misiepysie.graph_be;
 
 public class SourceFile {
-    private String sizeOfFile;
+    private double sizeOfFile;
     private String nameOfFile;
     private String connectionsOfFile; //todo: na razie string pozniej tzreba to jakos fajnie zapisac
 
-    SourceFile(String name,String size, String connections)
+    SourceFile(String name,double size, String connections)
     {
-
         sizeOfFile=size;
         nameOfFile=name;
         connectionsOfFile=connections;
