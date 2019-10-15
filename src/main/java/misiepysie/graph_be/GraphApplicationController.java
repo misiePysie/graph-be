61622c375b3e90e16a10b3fe56172ae3a5a11f1d
NWojt <n.wojtowicz098@gmail.com>
2019-10-15 21:52:17 +0200

    API with Frontend test

--- src/main/java/misiepysie/graph_be/GraphApplicationController.java
+++ src/main/java/misiepysie/graph_be/GraphApplicationController.java
@@ -3,6 +3,8 @@ package misiepysie.graph_be;
 import com.google.gson.Gson;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+
 
 @RestController
 public class GraphApplicationController {
@@ -15,9 +17,28 @@ public class GraphApplicationController {
 
       DirectoryPath directory = gson.fromJson(paths,DirectoryPath.class);
 
-        String test = "Hejka testujemy";
+//       System.out.println(directory.getBackendSrc()+'\n'+directory.getFrontendSrc());
+        Nodes node1 = new Nodes("./src/1","./src/java/lorem/ipsum","1","1234");
+        Nodes node2 = new Nodes("./src/2","Node 2","15","1234:");
+        Nodes node3 = new Nodes("./src/3","Node 3","50","12114");
 
-        System.out.println(directory.getBackendSrc()+'\n'+directory.getFrontendSrc());
-        return gson.toJson(test);
+        Edge edge1 = new Edge("./src/1","./src/3",1);
+        Edge edge2 = new Edge("./src/1","./src/2",1);
+        Edge edge3 = new Edge("./src/3","./src/3",7);
+
+        ArrayList<Nodes> nodesData= new ArrayList<Nodes>();
+        nodesData.add(node1);
+        nodesData.add(node2);
+        nodesData.add(node3);
+
+        ArrayList<Edge> edgesData= new ArrayList<Edge>();
+        edgesData.add(edge1);
+        edgesData.add(edge2);
+        edgesData.add(edge3);
+
+        Data data = new Data(nodesData,edgesData);
+
+
+        return gson.toJson(data);
     }
 }
